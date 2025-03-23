d36662377e02bbb0e0165af8f9c533da8a5c5194
Jasim Uddin <101209751+jasimuddinevan@users.noreply.github.com>
2025-03-24 00:27:07 +0600

    Update script.js

--- script.js
+++ script.js
@@ -4,29 +4,36 @@ function clock() {
         minutes: 0,
         seconds: 0,
         audio: new Audio('ticking-clock_1-27477.mp3'),
+        audioPermissionGranted: false,
         async startClock() {
-            try {
-                this.audio.volume = 0.5;
-                await this.audio.play();
-                this.audio.pause();
-            } catch (err) {
-                console.warn('Audio permission not granted or browser does not support audio:', err);
-            }
-            
-            setInterval(() => {
+            // Start the clock immediately without sound
+            const clockInterval = setInterval(() => {
                 const now = new Date();
                 this.hours = now.getHours() % 12;
                 this.minutes = now.getMinutes();
-                const newSeconds = now.getSeconds();
+                this.seconds = now.getSeconds();
                 
-                if (newSeconds !== this.seconds) {
-                    this.seconds = newSeconds;
+                // Only play sound if permission is granted
+                if (this.audioPermissionGranted && this.seconds !== this.lastSecond) {
+                    this.lastSecond = this.seconds;
                     try {
                         this.audio.currentTime = 0;
                         this.audio.play();
                     } catch (err) {}
                 }
             }, 1000);
+
+            // Request audio permission after 3 seconds
+            setTimeout(async () => {
+                try {
+                    this.audio.volume = 0.5;
+                    await this.audio.play();
+                    this.audio.pause();
+                    this.audioPermissionGranted = true;
+                } catch (err) {
+                    console.warn('Audio permission not granted or browser does not support audio:', err);
+                }
+            }, 3000);
         }
     }
 }
